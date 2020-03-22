4dcc0098f3014bf0ea6690c8acb38c53f80e8a82
h0341a <qianaiwuyu@gmail.com>
2020-03-22 11:27:02 +0800

    创建页头组件并全局引用

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-header height="56px">
+    <el-page-header
+      @back="goBack"
+      v-if="pageInfo.parent != null"
+      style="float:left; margin-top:16px"
+      title
+      :content="pageInfo.name"
+    ></el-page-header>
+    <span v-else style="font-size:18px;">{{pageInfo.name}}</span>
+    <div style="float:right; margin-top:10px">
+      <el-popover placement="bottom" v-model="popoverVisible">
+        <div style="text-align: right; margin: 0;">
+          <el-button size="mini" type="text" @click="popoverVisible = false">个人简介</el-button>
+          <br />
+          <el-button type="text" size="mini">注销</el-button>
+        </div>
+        <el-avatar slot="reference" :size="40"></el-avatar>
+      </el-popover>
+    </div>
+  </el-header>
+</template>
+<script>
+export default {
+  props: ["pageInfo"],
+  data() {
+    return {
+      popoverVisible: false
+    };
+  },
+  methods: {
+    goBack() {
+      this.$router.push(this.pageInfo.parent);
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
+import PageHeader from './components/PageHeader'
 
 Vue.use(ElementUI)
+Vue.component('PageHeader', PageHeader)
 
 Vue.config.productionTip = false
 
