c1bcfc26826e3229d06af1c6043a0b1c80300bcc
h0341a <qianaiwuyu@gmail.com>
2020-03-23 12:03:23 +0800

    将呈现书籍的卡片抽象为组件，重复使用

---
+++ src/components/BookCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-card shadow="hover">
+    <div>
+      <div style="float:left;width:46%">
+        <img
+        :src="require('../assets/little_women.jpg')"
+        title="点击查看详情"
+        :alt="book.name"
+        rel="v:photo"
+        style="width: 135px;height: 200px;"
+        />
+      </div>
+      <div class="book_describe">
+        书名:{{book.name}}
+        <br />作者: {{book.author}}
+        <br />副标题: {{book.subtitle}}
+        <br />多少人读过: {{book.fans}}
+        <br />评分: {{book.rank}}
+        <br />
+        <el-button type="text" plain>想读</el-button>
+        <span style="padding-left:30px;">
+          <el-button type="text" plain icon="el-icon-star-off">已读</el-button>
+        </span>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  props: ["book"]
+};
+</script>
+
+<style>
+.book_describe{
+  float: right;
+  width: 50%;
+  line-height: 30px;
+  font-size: 14px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,63 +9,15 @@
       <el-card class="box-card">
         <el-row>
           <el-col :span="6" v-for="o in 4" :key="o">
-            <el-card shadow="hover">
-              <div>
-                <div style="float:left;width:46%">
-                  <img
-                    :src="localBookImg"
-                    title="点击看大图"
-                    alt="小妇人"
-                    rel="v:photo"
-                    style="width: 135px;height: 200px;"
-                  />
-                </div>
-                <div class="book">
-                  书名:《小妇人》
-                  <br />作者: 路易莎·梅·奥尔科特
-                  <br />副标题: 无
-                  <br />原作名: Little Women
-                  <br />评分: 9.1
-                  <br />
-                  <el-button type="text" plain>想读</el-button>
-                  <span style="padding-left:30px;">
-                    <el-button type="text" plain icon="el-icon-star-off">已读</el-button>
-                  </span>
-                </div>
-              </div>
-            </el-card>
+            <BookCard v-bind:book="book"></BookCard>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="6" v-for="o in 4" :key="o">
-            <el-card shadow="hover">
-              <div>
-                <div style="float:left;width:46%">
-                  <img
-                    :src="localBookImg"
-                    title="点击看大图"
-                    alt="小妇人"
-                    rel="v:photo"
-                    style="width: 135px;height: 200px;"
-                  />
-                </div>
-                <div class="book">
-                  书名:《小妇人》
-                  <br />作者: 路易莎·梅·奥尔科特
-                  <br />副标题: 无
-                  <br />原作名: Little Women
-                  <br />评分: 9.1
-                  <br />
-                  <el-button type="text" plain>想读</el-button>
-                  <span style="padding-left:30px;">
-                    <el-button type="text" plain icon="el-icon-star-off">已读</el-button>
-                  </span>
-                </div>
-              </div>
-            </el-card>
+            <BookCard v-bind:book="book"></BookCard>
           </el-col>
         </el-row>
-        <div style="padding-left:26%;padding-top:10px;">
+        <div style="padding-left:30%;padding-top:10px;">
           <div style="float:left">
             <el-button
               v-if="pageNumber == 0"
@@ -76,7 +28,7 @@
             >上一页</el-button>
             <el-button v-else @click="pageNumber-=1" type="text" plain icon="el-icon-arrow-left">上一页</el-button>
           </div>
-          <div style="padding-right:34%;float:right">
+          <div style="padding-right:44%;float:right">
             <el-button @click="pageNumber+=1" type="text" plain icon="el-icon-search">刷新一下</el-button>
           </div>
         </div>
@@ -86,6 +38,7 @@
 </template>
 
 <script>
+import BookCard from "../components/BookCard";
 export default {
   data() {
     return {
@@ -96,8 +49,18 @@ export default {
       //单选按钮的选项，三种模式：随机(0),按评分(1)，按时间(2)
       bookListMode: "0",
       localBookImg: require("../assets/little_women.jpg"),
-      pageNumber: 0
+      pageNumber: 0,
+      book: {
+        name: "小妇人",
+        subtitle: "无",
+        author: "路易莎·梅·奥尔科特",
+        fans: 1723,
+        rank: 9.1
+      }
     };
+  },
+  components: {
+    BookCard
   }
 };
 </script>
@@ -108,11 +71,5 @@ export default {
 .el-col {
   padding-left: 10px;
   padding-top: 20px;
-}
-.book {
-  float: right;
-  width: 50%;
-  line-height: 30px;
-  font-size: 14px;
 }
 </style>
