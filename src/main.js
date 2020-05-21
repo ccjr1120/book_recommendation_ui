c89bc102d6c0bdc8d236f76a2090b2958c8ab50a
h0341a <qianaiwuyu@gmail.com>
2020-05-21 11:59:23 +0800

    添加vuex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import PageHeader from './components/PageHeader'
+import store from './store'
 
 Vue.use(ElementUI)
 Vue.component('PageHeader', PageHeader)
@@ -12,5 +13,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
